4f1d9a30c53828aabfabba6af2251761a97b0fa3
uchizumimasato <64341031+u-mma@users.noreply.github.com>
2022-08-21 23:42:11 +0900

    リファクタ

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Blog } from "./blog"
-import { Admin } from "./Admin/AdminPage"
+import { Admin } from "./Admin"
 
-export class AppRouter extends React.Component {
-  render() {
-    return (
-      <Routes>
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-    )
-  }
+export const AppRouter: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/admin">
+        <Admin />
+      </Route>
+      <Route path="/blog">
+        <Blog />
+      </Route>
+    </Switch>
+  )
 }
